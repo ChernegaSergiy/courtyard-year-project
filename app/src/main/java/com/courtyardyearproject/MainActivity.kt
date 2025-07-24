31f0d6cc60b16a7a27bb5ca9eb11b56fef6b29b4
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-07-24 13:29:20 +0300

    fix: Correctly set application and activity titles

--- app/src/main/java/com/courtyardyearproject/MainActivity.kt
+++ app/src/main/java/com/courtyardyearproject/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTitle(R.string.main_activity_name)
 
         cameraExecutor = Executors.newSingleThreadExecutor()
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
