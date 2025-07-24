34394d960e93529ae32ae6b1d2b8ec2e24ca1559
Serhii Cherneha <chernegasergiy3@gmail.com>
2025-07-24 11:37:21 +0300

    fix: Add missing Environment import

--- app/src/main/java/com/courtyardyearproject/CaptureService.kt
+++ app/src/main/java/com/courtyardyearproject/CaptureService.kt
@@ -7,6 +7,7 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.Build
+import android.os.Environment
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
